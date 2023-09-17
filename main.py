c0354e7d1e1363139e4f94bd1c035e7417ea4a96
Martin Münch <minarth@gmail.com>
2023-09-17 12:17:55 +0800

    Task 3a

--- main.py
+++ main.py
@@ -1,8 +1,79 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+from datamodel import Client, ClientRepository, ProductRepository, ProductClientRepository
 
 app = FastAPI()
 
+client_repo = ClientRepository()
+prod_repo = ProductRepository()
+prod_client_repo = ProductClientRepository()
 
-@app.get("/")
+
+class ClientInputModel(BaseModel):
+    first_name: str
+    last_name: str
+
+
+@app.get("/status")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "OK"}
+
+
+@app.get("/clients")
+async def get_clients():
+    return client_repo.data
+
+
+@app.get("/clients/{client_uid}")
+async def get_client(client_uid: int):
+    # TODO: Lookup query param without trailing /
+    if client_uid not in client_repo.data:
+        raise HTTPException(status_code=404, 
+                detail=f"Client with ID `{client_uid}` not found")
+    client = client_repo.data[client_uid]
+    return client
+
+
+@app.get("/products")
+async def get_products():
+    return prod_repo.data
+
+
+@app.get("/products/{product_uid}")
+async def get_product(product_uid: int):
+    if product_uid not in prod_repo.data:
+        raise HTTPException(status_code=404, 
+                detail=f"Product with ID `{product_uid}` not found")
+    return prod_repo.data[product_uid]
+
+
+@app.post("/clients")
+async def create_client(client_data: ClientInputModel):
+    client_repo.add(None, Client(None, 
+                                 first_name=client_data.first_name,
+                                 last_name=client_data.last_name))
+    return client_data
+
+
+@app.get("/clients/{client_uid}/products/{product_uid}/balance")
+async def get_client_balance(client_uid: int, product_uid: int):
+    balances = {}
+    for acc_number, value in prod_client_repo.data.items():
+        if value.client_uid == client_uid and value.product_uid == product_uid:
+            # TODO without specific balances[acc_number] = {"balance": value.params["balance"]}
+            balances[acc_number] = {"balance": value.params["balance"],
+                                    "currency": value.params["currency"]}
+            
+    return balances
+
+
+
+
+
+
+
+
+
+
+
