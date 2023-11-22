13f1dd7184f71ef90428e58af12eb3d64528ff7a
Martin Münch <minarth@gmail.com>
2023-11-22 10:38:51 +0100

    Gets on product, client. Find user by name. Simple error handling

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from datamodel import ClientRepository
+from datamodel import ClientRepository, ProductRepository
 
 app = FastAPI()
 
 clients_repo = ClientRepository()
+product_repo = ProductRepository()
 
 @app.get("/status")
 async def root():
@@ -15,3 +16,34 @@ async def root():
 async def get_clients():
     return clients_repo.data
 
+
+# Task 1a - prepare get products endpoint.
+@app.get("/products")
+async def get_products():
+    return product_repo.data
+
+
+@app.get("/clients/{id_client}")
+async def get_client(id_client: int):
+    if id_client not in clients_repo.data:
+        raise HTTPException(404, f"Client with `{id_client}` ID not found")
+    return clients_repo.data[id_client]
+
+# Task1b: GET Product specific data
+# Bonus: GET Client by name
+
+@app.get("/products/{id_product}")
+async def get_product(id_product: int):
+    if id_product not in product_repo.data:
+        raise HTTPException(404, f"Product with `{id_product}` ID not found")
+    return product_repo.data[id_product]
+
+
+@app.get("/find-clients")
+async def find_clients(query: str):
+    found = []
+    for client in clients_repo.data.values():
+        if query in (client.first_name, client.last_name):
+            found.append(client)
+    
+    return {"clients_found": found}
